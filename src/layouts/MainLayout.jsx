5890fc43e5de062176b60da82f5b40a869bfa211
Rafidul01 <rafidulhasan555@gmail.com>
2024-06-22 22:07:43 +0600

    footer section complite

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../pages/Shared/Navbar/Navbar";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Footer from "../pages/Shared/Footer/Footer";
 
 const MainLayout = () => {
     return (
         <div>
             <Navbar/>
             <Outlet/>
+            <Footer/>
             <ToastContainer  position="top-center"/>
             
         </div>
